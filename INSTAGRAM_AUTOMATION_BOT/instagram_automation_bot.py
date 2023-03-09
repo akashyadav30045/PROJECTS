e890868bcf08ff576bc41835f886ce540f5b6181
akashyadav30045 <119119145+akashyadav30045@users.noreply.github.com>
2023-03-09 09:54:29 +0530

    Add files via upload

---
+++ INSTAGRAM_AUTOMATION_BOT/instagram_automation_bot.py
@@ -0,0 +1,8 @@
+from instabot import Bot
+bot=Bot()
+bot.login(username="yadavrocks123456", password="akash9211" )
+#bot.follow('python.hub')
+
+#to upload photo
+
+bot.upload_photo("C:/Users/akash yadav/Pictures/insta.jpg", caption="i love python")
